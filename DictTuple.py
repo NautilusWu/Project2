dfe236d03524582031974af0d14e9f612167c117
Yunze Wu <yunzew3@uci.edu>
2024-08-04 00:35:42 -0700

    first ics33

---
+++ DictTuple.py
@@ -0,0 +1,235 @@
+"""
+SCI 33 Project 2: Customized namedtuple class DictTuple
+"""
+
+from mynamedtuple import mynamedtuple
+
+
+class DictTuple:
+
+    def __init__(self, *args):
+        self.dt = list(args)
+        if (
+            any(not isinstance(x, dict) for x in self.dt)
+            or any(len(x) == 0 for x in self.dt)
+            or len(self.dt) == 0
+        ):
+            raise AssertionError(
+                "All input must be dictionaries with at least one element"
+            )
+
+    def __len__(self):
+        distinct_keys = {key for item in self.dt for key in item}
+        return len(distinct_keys)
+
+    def __bool__(self):
+        return len(self.dt) > 1
+
+    def __repr__(self):
+        str_01 = (
+            f"{self.__class__.__name__}({', '.join(str(x) for x in self.dt)})"
+        )
+        return str_01
+
+    def __contains__(self, item):
+        distinct_keys = {key for item in self.dt for key in item}
+        return item in distinct_keys
+
+    def __getitem__(self, key):
+        distinct_keys = {key for item in self.dt for key in item}
+        if key not in distinct_keys:
+            raise KeyError(f"Key '{key}' does not exist")
+        latest_dict = [item for item in self.dt if key in item][-1]
+        return latest_dict[key]
+
+    def __setitem__(self, key, value):
+        distinct_keys = {key for item in self.dt for key in item}
+        if key not in distinct_keys:
+            self.dt.append({key: value})
+        else:
+            max_inx = max(
+                inx for inx, item in enumerate(self.dt) if key in item
+            )
+            self.dt[max_inx][key] = value
+
+    def __delitem__(self, key):
+        distinct_keys = {key for item in self.dt for key in item}
+        if key not in distinct_keys:
+            raise KeyError(f"Key '{key}' does not exist")
+        self.dt = [item for item in self.dt if key not in item]
+
+    def __call__(self, key):
+        distinct_keys = {key for item in self.dt for key in item}
+        if key not in distinct_keys:
+            return []
+        lst_01 = [item.get(key) for item in self.dt if key in item]
+        lst_02 = [
+            len(item.get(key)._fields) for item in self.dt if key in item
+        ]
+        # print(lst_01, lst_02)
+        return [
+            [value[i] for i in range(lst_02[inx])]
+            for inx, value in enumerate(lst_01)
+        ]
+
+    def __iter__(self):
+        keys_list = [key for item in self.dt for key in item]
+        distinct_keys = sorted(set(keys_list), key=keys_list.index)
+        my_iter_01 = []
+        for k in distinct_keys:
+            my_iter_01.append([item for item in self.dt if k in item][-1])
+        my_iter_02 = []
+        for item_dict in my_iter_01:
+            temp_dict = {}
+            for k in sorted(item_dict):
+                temp_dict[k] = item_dict[k]
+            my_iter_02.append(temp_dict)
+        # print(distinct_keys)
+        # print(my_iter_02)
+        return iter(my_iter_02)
+
+    def __eq__(self, other):
+        if not isinstance(other, (DictTuple, dict)):
+            return False
+        if len(self) != len(other):
+            return False
+        distinct_keys_01 = sorted(
+            list({key for item in self.dt for key in item})
+        )
+        if isinstance(other, DictTuple):
+            distinct_keys_02 = sorted(
+                list({key for item in other.dt for key in item})
+            )
+            # print(distinct_keys_01, distinct_keys_02)
+            if distinct_keys_01 != distinct_keys_02:
+                return False
+            # for k in distinct_keys_01:
+            #     print(self[k], other[k])
+            return all(self[k] == other[k] for k in distinct_keys_01)
+        if not isinstance(other, dict):
+            return False
+        if any(k not in self for k in other.keys()):
+            return False
+        return all(self[k] == other[k] for k in distinct_keys_01)
+
+    def __add__(self, other):
+        if not isinstance(other, (DictTuple, dict)):
+            raise TypeError(f"Can not add type '{other.__class__.__name__}'")
+        if isinstance(other, DictTuple):
+            return DictTuple(*self.dt, *other.dt)
+        return DictTuple(*self.dt, other)
+
+    def __radd__(self, other):
+        if not isinstance(other, (DictTuple, dict)):
+            raise TypeError(f"Can not add type '{other.__class__.__name__}'")
+        return DictTuple(other) + self
+
+    def __iadd__(self, other):
+        if not isinstance(other, (DictTuple, dict)):
+            raise TypeError(f"Can not add type '{other.__class__.__name__}'")
+        if isinstance(other, DictTuple):
+            self.dt = [*self.dt, *other.dt]
+        else:
+            self.dt = [*self.dt, other]
+        return self
+
+    def __setattr__(self, name, value):
+        if (
+            not isinstance(value, list)
+            or len(value) == 0
+            or any(len(x) == 0 or not isinstance(x, dict) for x in value)
+        ):
+            raise AssertionError(
+                "value must be dictionaries with at least one element"
+            )
+        if name != "dt":
+            raise AssertionError("Can not set attribute except 'dt'")
+        # if hasattr(self,'dt'):
+        #     return DictTuple({name: value})
+        return super().__setattr__(name, value)
+
+
+# Testing the DictTuple class
+def main():
+    coordinate = mynamedtuple("coordinate", "x y")
+    d = DictTuple({"c1": coordinate(1, 2)}, {"c1": coordinate(3, 4)})
+    # d = DictTuple({"c1": "AAA"}, {"c1": "BBB"}, {"c2": "CCC"}, {"c1": "DDD"})
+    # d = DictTuple({'c1': 'AAA'})
+    # d = DictTuple(1)
+    print(len(d))
+    print(d.dt)
+    print(bool(d))
+    print(d)
+    print("c1" in d)
+    print("c2" in d)
+    print(d["c1"])
+    # print(d["c3"])
+    d["c1"] = "FFFF"
+    print(d["c1"])
+    print(d.dt)
+    d["c3"] = "GGGG"
+    print(d.dt)
+    del d["c1"]
+    print(d.dt)
+    # del d["c1"]
+    e = DictTuple(
+        {"f1": coordinate(1, 2), "c5": coordinate(7, 8)},
+        {"c1": coordinate(3, 4), "c2": coordinate(9, 10)},
+        {"c1": coordinate("AAA", "BBB")},
+    )
+    print(f"e.dt: {e.dt}")
+    print(f"e('c1'): {e('c1')}")
+    print(f"e('c2'): {e('c2')}")
+    print(f"e['c1']: {e['c1']}")
+    print(f"e['c2']: {e['c2']}")
+    print("===== iter =====")
+    for i, item in enumerate(e):
+        print(i, item)
+    e2 = DictTuple(
+        {"c1": coordinate(3, 4), "c2": coordinate(9, 10)},
+        {"f1": coordinate(1, 2), "c5": coordinate(7, 8)},
+        {"c1": coordinate("AAA", "BBB")},
+    )
+    print("==============")
+    print(f"e == e2: {e == e2}")
+    dict_01 = {
+        "c2": coordinate(9, 10),
+        "f1": coordinate(1, 2),
+        "c5": coordinate(7, 8),
+        "c1": coordinate("AAA", "BBB"),
+        # "c3": "GGGG",
+    }
+    print("==============")
+    print(f"e == dict_01: {e == dict_01}")
+    print(f"e2 == dict_01: {e2 == dict_01}")
+    print(f"e2 == 'ABCD': {e2 == 'ABCD'}")
+
+    print("========= + =========")
+    d1 = DictTuple({"c1": coordinate(1, 2)}, {"c1": coordinate(3, 4)})
+    d2 = DictTuple({"c2": coordinate(1, 2)}, {"c3": coordinate(3, 4)})
+    d3 = d1 + d2
+    print(d3.dt)
+    d4 = d2 + d1
+    print(d4.dt)
+    adt = DictTuple({"c1": coordinate(1, 2)}, {"c1": coordinate(3, 4)})
+    adict = {"c3": coordinate(3, 4)}
+    print(adt + adict)
+    print(adict + adt)
+    # print(adt + 'ABCD')
+    print("========= += =========")
+    d1 = DictTuple({"c1": coordinate(1, 2)}, {"c1": coordinate(3, 4)})
+    d2 = DictTuple({"c2": coordinate(1, 2)}, {"c3": coordinate(3, 4)})
+    adict = {"c3": coordinate(3, 4)}
+    d1 += d2
+    print(d1)
+    d1 += adict
+    print(d1)
+    # d1 += 'ABCD'
+    print(d1.dt)
+    # setattr(d1, "dt", [{'ss' : coordinate(50, 60)}])
+    setattr(d1, "dt", [{"s": "s"}])
+    print(d1)
+
+
+if __name__ == "__main__":
+    main()
---
+++ mynamedtuple.py
@@ -0,0 +1,236 @@
+"""
+SCI 33 Project 2: Customized namedtuple class generator
+"""
+
+from keyword import kwlist
+
+
+def chk_field_names(field_names) -> bool:
+    """
+    Check if the field names are valid identifiers.
+    """
+    rtn = False
+    if isinstance(field_names, list):
+        if all(
+            isinstance(name, str)
+            and name.isidentifier()
+            and (name not in kwlist)
+            for name in field_names
+        ):
+            rtn = True
+    if isinstance(field_names, str):
+        if "," in field_names:
+            field_names_lst = field_names.split(",")
+            field_names_lst = [name.strip() for name in field_names_lst]
+        else:
+            field_names_lst = field_names.split()
+        if all(
+            isinstance(field_name, str)
+            and field_name.isidentifier()
+            and field_name not in kwlist
+            for field_name in field_names_lst
+        ):
+            rtn = True
+    return rtn
+
+
+def mynamedtuple(type_name, field_names, mutable=False, defaults=None):
+    """
+    Generates customized namedtuple classes dynamically.
+
+    Args:
+        type_name (str): The name of the class.
+        field_names (str or list): A string or a list of strings representing
+                                   the field names.
+        mutable (bool, optional): Whether the class is mutable or not.
+                                  Defaults to False.
+        defaults (dict, optional): A dictionary of default values for the
+                                   fields. Defaults to {}.
+
+    Returns:
+        A customized namedtuple class.
+    """
+    # print(type_name, field_names, mutable, defaults)
+
+    type_name_check = (
+        isinstance(type_name, str)
+        and type_name.isidentifier()
+        and type_name not in kwlist
+    )
+    field_names_check = chk_field_names(field_names)
+    if defaults is None:
+        defaults = {}
+        defaults_check = True
+    else:
+        defaults_check = isinstance(defaults, dict) and all(
+            isinstance(key, str) and key.isidentifier()  # Should we allow it?
+            for key in defaults.keys()
+        )
+    if mutable is None:
+        mutable = False
+    mutable_check = isinstance(mutable, bool)
+    # print(type_name_check, field_names_check, defaults_check, mutable_check)
+    if not all(
+        [type_name_check, field_names_check, defaults_check, mutable_check]
+    ):
+        raise SyntaxError(
+            "Type names, field names and defaults must be valid identifiers"
+        )
+
+    if not isinstance(field_names, list):
+        if "," in field_names:
+            field_names = field_names.split(",")
+            field_names = [name.strip() for name in field_names]
+        else:
+            field_names = field_names.split()
+    field_names = sorted(set(field_names), key=field_names.index)
+    if not all(key in field_names for key in defaults.keys()):
+        raise SyntaxError("Default values must correspond to field names")
+
+    # cls_name = "MyNamedTuple_" + type_name
+    cls_name = type_name
+    my_code = f"class {cls_name}:\n"
+    my_code += f"    _fields = {field_names}\n"
+    my_code += f"    _defaults = {defaults}\n"
+    my_code += f"    _mutable = {mutable}\n"
+    init_lst = []
+    for name in field_names:
+        if name in defaults.keys():
+            if isinstance(defaults[name], str):
+                init_lst.append(f"{name}='{defaults[name]}'")
+            else:
+                init_lst.append(f"{name}={defaults[name]}")
+        else:
+            init_lst.append(f"{name}")
+    init_str = ", ".join(init_lst)
+    my_code += f"    def __init__(self, {init_str}):\n"
+    for name in field_names:
+        my_code += f"        self.{name} = {name}\n"
+
+    my_code += f"    def __repr__(self):\n"
+    my_code += f"        lst = [(\n"
+    my_code += f"            f'{{k}} = \\'{{v}}\\''\n"
+    my_code += f"            if isinstance(v, str)\n"
+    my_code += f"            else f'{{k}}={{v}}')\n"
+    my_code += f"            for k, v in self.__dict__.items()]\n"
+    my_code += f"        str1 =', '.join(lst)\n"
+    my_code += f"        return f'{cls_name}({{str1}})'\n"
+
+    for attr in field_names:
+        my_code += f"    def get_{attr}(self):\n"
+        my_code += f"        return self.{attr}\n"
+
+    my_code += f"    def __getitem__(self, index):\n"
+    my_code += f"        if index < 0 or index >= len(self._fields):\n"
+    my_code += f"            raise IndexError('index out of range')\n"
+    my_code += f"        return getattr(self, self._fields[index])\n"
+
+    my_code += f"    def __eq__(self, other):\n"
+    my_code += f"        if not isinstance(other, self.__class__):\n"
+    my_code += f"            return False\n"
+    my_code += f"        if self.__class__!= other.__class__:\n"
+    my_code += f"            return False\n"
+    my_code += f"        if len(self._fields) != len(other._fields):\n"
+    my_code += f"            return False\n"
+    my_code += f"        for name in self._fields:\n"
+    my_code += f"            if getattr(self, name) != getattr(other, name):\n"
+    my_code += f"                return False\n"
+    my_code += f"        return True\n"
+
+    my_code += f"    def _asdict(self):\n"
+    my_code += (
+        f"        return dict({{k: getattr(self, k) for k in self._fields}})\n"
+    )
+
+    my_code += f"    def _make(self, iterable):\n"
+    my_code += f"        if len(iterable) != len(self._fields):\n"
+    my_code += f"            raise TypeError(f'Expected {{len(self._fields)}} arguments')\n"
+    my_code += f"        return self.__class__(*iterable)\n"
+
+    my_code += f"    def _replace(self, **kargs):\n"
+    # my_code += f"        print(kargs)\n"
+    my_code += (
+        f"        if not all(k in self._fields for k in kargs.keys()):\n"
+    )
+    my_code += f"            raise ValueError('Invalid field names')\n"
+    my_code += f"        if self._mutable:\n"
+    my_code += f"            for name in kargs.keys():\n"
+    my_code += f"                setattr(self, name, kargs[name])\n"
+    my_code += f"            return None\n"
+    my_code += f"        else:\n"
+    my_code += f"           new_args = []\n"
+    my_code += f"           for name in self._fields:\n"
+    my_code += f"               if name in kargs:\n"
+    my_code += f"                   new_args.append(kargs[name])\n"
+    my_code += f"               else:\n"
+    my_code += f"                   new_args.append(getattr(self, name))\n"
+    my_code += f"           return self.__class__(*new_args)\n"
+
+    my_code += f"    def __setattr__(self, name, value):\n"
+    my_code += f"        if (\n"
+    my_code += f"            not self._mutable\n"
+    my_code += f"            and (hasattr(self, name)\n"
+    my_code += f"               or name not in self._fields)\n"
+    my_code += f"        ):\n"
+    my_code += f"            raise AttributeError(f'{cls_name} object is immutable')\n"
+    my_code += f"        super().__setattr__(name, value)\n"
+
+    # print(my_code)
+    exec(my_code)
+    # print(f'\n--------\n{locals()}\n----------\n')
+    return locals()[cls_name]
+
+
+def main():
+    """
+    Test the mynamedtuple function
+    """
+    Point = mynamedtuple("Point", "x y z", defaults={"z": 0})
+    Point2 = mynamedtuple(
+        "Point2", "x  y z k", defaults={"y": 0, "z": 0, "k": "BB"}
+    )
+    # print(Point.__dict__)
+    p = Point("AAA", 1)
+    print(repr(p))
+    print(p[1])
+    p2 = Point2("AAA")
+    print(repr(p2))
+    print(p == p2)
+    print(p2._asdict())
+    p3 = p._make("12S")
+    print(p3._asdict())
+    # print(p3.__class__.__dict__)
+    p4 = p._replace(x="BBB")
+    print(p4)
+    p5 = p._replace(x="BBB", y=100)
+    print(p5)
+    Point3 = mynamedtuple("Point3", "x y", mutable=False, defaults={"y": 1})
+    p6 = Point3(1)
+    print(p6._asdict())
+    p7 = p6._replace(x=2)
+    print(p6, p7)
+    Point4 = mynamedtuple("Point4", "x y", mutable=True, defaults={"y": 1})
+    p8 = Point4(1)
+    print(p8._asdict())
+    p9 = p8._replace(x=2)
+    print(p8, p9)
+    NamedTuple5 = mynamedtuple(
+        "NamedTuple5", "x y  z", mutable=True, defaults={"z": "ZZZ"}
+    )
+    nt10 = NamedTuple5(1, 2)
+    print(nt10)
+    setattr(nt10, "x", 100)
+    print(nt10)
+    setattr(nt10, "x1", 300)
+    print(nt10)
+    NamedTuple6 = mynamedtuple(
+        "NamedTuple6", "x y  z", mutable=False, defaults={"z": "ZZZ"}
+    )
+    nt11 = NamedTuple6(1, 2)
+    print(nt11)
+    setattr(nt11, "x1", 200)
+    print(nt11)
+
+
+if __name__ == "__main__":
+    main()
