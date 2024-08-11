90c1073a11bb119aab46710e85786c74b47ff710
Yunze Wu <yunzew3@uci.edu>
2024-08-10 23:19:20 -0700

    modify

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -158,10 +158,10 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults=None):
     my_code += f"        return self.__class__(*iterable)\n"
 
     my_code += f"    def _replace(self, **kargs):\n"
-    my_code += f"        print(kargs)\n"
-    my_code += f"        print(self._fields)\n"
-    my_code += f"        print(self._defaults)\n"
-    my_code += f"        print(self._mutable)\n"
+    my_code += f"        print('kargs:', kargs)\n"
+    my_code += f"        print('fields:',self._fields)\n"
+    my_code += f"        print('defaults:', self._defaults)\n"
+    my_code += f"        print('mutable:',self._mutable)\n"
     my_code += (
         f"        if not all(k in self._fields for k in kargs.keys()):\n"
     )
