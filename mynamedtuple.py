a7ecca0727103e2f32ee3824038954d882d170f5
Yunze Wu <yunzew3@uci.edu>
2024-08-10 22:41:19 -0700

    modify

--- mynamedtuple.py
+++ mynamedtuple.py
@@ -143,7 +143,7 @@ def mynamedtuple(type_name, field_names, mutable=False, defaults=None):
     )
 
     my_code += f"    def _make(self, iterable=()):\n"
-    my_code += f"       print(iterable)\n"
+    my_code += f"        print(iterable)\n"
     # my_code += f"        if not iterable:\n"
     # my_code += f"           print('dllm')\n"
     # my_code += f"           raise TypeError('Iterable must have something')\n"
